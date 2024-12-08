241281e8991d34844a84ec826a37f70ae59cf52f
Babji Polisetti <babji7819@gmail.com>
2024-12-08 19:40:40 +0530

    Excel Splitter UI Update

--- src/main/java/com/commonUtils/controller/ExcelUtilController.java
+++ src/main/java/com/commonUtils/controller/ExcelUtilController.java
@@ -2,12 +2,22 @@ package com.commonUtils.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
 
 @Controller
 public class ExcelUtilController {
 	
 	@GetMapping("/excel-Splitter")
 	public String excelSplitter() {
+		return "Excel-Utils/ExcelSplitter";
+	}
+	
+	@PostMapping("/excel-Splitter")
+	public String excelSplitterProcesser(@RequestParam("file") MultipartFile file,
+            @RequestParam("columns") String columns,
+            @RequestParam("outputFormat") String outputFormat) {
 		return "Excel-Utils/ExcelSplitter";
 	}
 
