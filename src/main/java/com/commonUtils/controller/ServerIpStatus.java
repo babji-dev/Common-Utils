13cea16d398801a6ad29f7af68c4e8cb0182a390
Babji Polisetti <babji7819@gmail.com>
2025-04-07 21:36:47 +0530

    Added Property to Test

--- src/main/java/com/commonUtils/controller/ServerIpStatus.java
+++ src/main/java/com/commonUtils/controller/ServerIpStatus.java
@@ -2,6 +2,7 @@ package com.commonUtils.controller;
 
 import com.commonUtils.utils.NetworkTester;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,9 +14,12 @@ public class ServerIpStatus {
     @Autowired
     private NetworkTester networkTester;
 
+    @Value("${data.test}")
+    private String testValue;
+
     @GetMapping("/getIp")
     public String getIp() throws Exception {
-        return networkTester.getPrimaryIp();
+        return testValue;
     }
 
 }
